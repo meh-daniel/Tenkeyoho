608a27caa36352ff9cb56aea5704e6901b793f9f
Danil Galkin <mehanikdanil@gmail.com>
2022-07-09 15:32:25 +0700

    create base screens application and add navigation component

---
+++ app/src/main/java/meh/daniel/com/tenkeyoho/presentation/infoweather/InfoWeatherFragment.kt
@@ -0,0 +1,7 @@
+package meh.daniel.com.tenkeyoho.presentation.infoweather
+
+import androidx.fragment.app.Fragment
+import meh.daniel.com.tenkeyoho.R
+
+class InfoWeatherFragment : Fragment(R.layout.fragment_infoweather) {
+}
---
+++ app/src/main/java/meh/daniel/com/tenkeyoho/presentation/mainscreen/MainScreenFragment.kt
@@ -0,0 +1,22 @@
+package meh.daniel.com.tenkeyoho.presentation.mainscreen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import meh.daniel.com.tenkeyoho.R
+import meh.daniel.com.tenkeyoho.databinding.FragmentMainscreenBinding
+
+class MainScreenFragment : Fragment(R.layout.fragment_mainscreen) {
+
+    private lateinit var binding: FragmentMainscreenBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainscreenBinding.bind(view)
+        binding.openScreenInfoWeather.setOnClickListener {
+            findNavController().navigate(R.id.action_mainScreenFragment_to_infoWeatherFragment)
+        }
+    }
+
+}
