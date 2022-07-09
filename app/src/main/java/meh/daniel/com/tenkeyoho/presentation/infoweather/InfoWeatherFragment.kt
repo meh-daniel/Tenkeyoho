a97bbfb0d0cc5684ce7e15ffc21fb63f1df59135
Danil Galkin <mehanikdanil@gmail.com>
2022-07-09 18:39:44 +0700

    refactoring

--- app/src/main/java/meh/daniel/com/tenkeyoho/presentation/infoweather/InfoWeatherFragment.kt
+++ app/src/main/java/meh/daniel/com/tenkeyoho/presentation/infoweather/InfoWeatherFragment.kt
@@ -1,7 +1,14 @@
 package meh.daniel.com.tenkeyoho.presentation.infoweather
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import meh.daniel.com.tenkeyoho.R
 
 class InfoWeatherFragment : Fragment(R.layout.fragment_infoweather) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
