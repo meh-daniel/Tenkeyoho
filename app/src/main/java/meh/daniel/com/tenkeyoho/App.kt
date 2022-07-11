5a11239edba07db94aa67bc7bd992fbb12fd3cb6
Danil Galkin <mehanikdanil@gmail.com>
2022-07-11 12:20:39 +0700

    реализация repository

---
+++ app/src/main/java/meh/daniel/com/tenkeyoho/App.kt
@@ -0,0 +1,26 @@
+package meh.daniel.com.tenkeyoho
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import meh.daniel.com.tenkeyoho.data.WeatherApi
+import meh.daniel.com.tenkeyoho.data.WeatherRepositoryImpl
+import meh.daniel.com.tenkeyoho.domain.WeatherRepository
+
+@HiltAndroidApp
+class App : Application(){
+
+    companion object{
+        lateinit var weatherRepository: WeatherRepository
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initRepository()
+    }
+
+    private fun initRepository() {
+        weatherRepository = WeatherRepositoryImpl(weatherApi = WeatherApi.createApi())
+    }
+
+}
