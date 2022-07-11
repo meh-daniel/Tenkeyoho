5308128177518b92efaf0b2f25ce379f444a43b7
Danil Galkin <mehanikdanil@gmail.com>
2022-07-11 12:15:25 +0700

    Создание repository для network api

---
+++ app/src/main/java/meh/daniel/com/tenkeyoho/data/WeatherRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package meh.daniel.com.tenkeyoho.data
+
+import meh.daniel.com.tenkeyoho.data.model.WeathersNW
+import meh.daniel.com.tenkeyoho.domain.WeatherRepository
+
+
+const val APP_ID = "22e2d18f9cd7316c5de47fd73d2ae414"
+const val CITY = "London"
+const val UNITS = "metric"
+const val LANG = "ru"
+
+class WeatherRepositoryImpl(private val weatherApi: WeatherApi) : WeatherRepository {
+    override suspend fun getWeather(): WeathersNW {
+        return weatherApi.getWeathers(appid = APP_ID, city = CITY, units = UNITS, lang = LANG)
+    }
+
+}
---
+++ app/src/main/java/meh/daniel/com/tenkeyoho/domain/WeatherRepository.kt
@@ -0,0 +1,9 @@
+package meh.daniel.com.tenkeyoho.domain
+
+import meh.daniel.com.tenkeyoho.data.model.WeathersNW
+
+interface WeatherRepository {
+
+    suspend fun getWeather() : WeathersNW
+
+}
