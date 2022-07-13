8a43d3116c071ac3e6671b649064ce7979bf0a27
Danil Galkin <mehanikdanil@gmail.com>
2022-07-13 15:19:30 +0700

    refactoring pull request. Изменение имени метода на более открыто говорящий

--- app/src/main/java/meh/daniel/com/tenkeyoho/data/WeatherApi.kt
+++ app/src/main/java/meh/daniel/com/tenkeyoho/data/WeatherApi.kt
@@ -1,14 +1,10 @@
 package meh.daniel.com.tenkeyoho.data
 
 import meh.daniel.com.tenkeyoho.data.model.WeathersNW
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -16,16 +12,16 @@ const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
 interface WeatherApi {
     companion object{
-        val logging: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        private val logging: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
             this.level = HttpLoggingInterceptor.Level.BASIC
         }
-        fun client() = OkHttpClient.Builder().apply {
+        private fun createLoggingOkHttpClient() = OkHttpClient.Builder().apply {
             addInterceptor(logging)
         }.build()
         fun createApi() : WeatherApi{
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .client(client())
+                .client(createLoggingOkHttpClient())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             return retrofit.create(WeatherApi::class.java)
