4d13b2aa7e1126ca4601d5f3c53a98a164550577
Danil Galkin <mehanikdanil@gmail.com>
2022-07-13 15:53:21 +0700

    refactoring pull request. Запривачивание констант

--- app/src/main/java/meh/daniel/com/tenkeyoho/data/WeatherRepositoryImpl.kt
+++ app/src/main/java/meh/daniel/com/tenkeyoho/data/WeatherRepositoryImpl.kt
@@ -4,10 +4,10 @@ import meh.daniel.com.tenkeyoho.data.model.WeathersNW
 import meh.daniel.com.tenkeyoho.domain.WeatherRepository
 
 
-const val APP_ID = "22e2d18f9cd7316c5de47fd73d2ae414"
-const val CITY = "London"
-const val UNITS = "metric"
-const val LANG = "ru"
+private const val APP_ID = "22e2d18f9cd7316c5de47fd73d2ae414"
+private const val CITY = "London"
+private const val UNITS = "metric"
+private const val LANG = "ru"
 
 class WeatherRepositoryImpl(private val weatherApi: WeatherApi) : WeatherRepository {
     override suspend fun getWeather(): WeathersNW {
