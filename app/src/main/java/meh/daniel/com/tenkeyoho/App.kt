471d59e5a4ba0fced59aa7069a4d5aa9656ef264
Danil Galkin <mehanikdanil@gmail.com>
2022-07-20 07:41:11 +0700

    Создание базы данных через RepositoryImpl

--- app/src/main/java/meh/daniel/com/tenkeyoho/App.kt
+++ app/src/main/java/meh/daniel/com/tenkeyoho/App.kt
@@ -1,20 +1,26 @@
 package meh.daniel.com.tenkeyoho
 
 import android.app.Application
-import meh.daniel.com.tenkeyoho.data.WeatherApi
-import meh.daniel.com.tenkeyoho.data.network.WeatherNetworkRepositoryImpl
-import meh.daniel.com.tenkeyoho.domain.network.WeatherNetworkRepository
+import meh.daniel.com.tenkeyoho.data.WeatherRepositoryImpl
+import meh.daniel.com.tenkeyoho.data.db.AppDataBase
+import meh.daniel.com.tenkeyoho.data.nw.WeatherApi
+import meh.daniel.com.tenkeyoho.domain.WeatherRepository
 
 class App : Application(){
+
     companion object{
-        lateinit var weatherNetworkRepository: WeatherNetworkRepository
+        lateinit var weatherRepository: WeatherRepository
             private set
     }
+
     override fun onCreate() {
         super.onCreate()
         initRepository()
     }
+
     private fun initRepository() {
-        weatherNetworkRepository = WeatherNetworkRepositoryImpl(weatherApi = WeatherApi.createApi())
+        weatherRepository = WeatherRepositoryImpl(weatherApi = WeatherApi.createApiOpenWeatherMap(),
+            appDataBase = AppDataBase.createAppDataBase(this))
     }
+
 }
